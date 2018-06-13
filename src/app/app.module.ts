894d95decce35db16eded025df537dcc332d12b5
ecreyes <ereyesoyarzo@gmail.com>
2018-06-13 13:00:58 -0400

    se agregó la barra de navegación con las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTING } from "./app.routes";
+
+//Servicios
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
@@ -14,7 +20,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
