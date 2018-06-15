4cf3f774769042483f70fcbc4027caca63553f66
ecreyes <ereyesoyarzo@gmail.com>
2018-06-15 01:05:10 -0400

    aplicación terminada

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { SpotifyService } from "../../services/spotify.service";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-artist',
@@ -8,6 +9,7 @@ import { SpotifyService } from "../../services/spotify.service";
 })
 export class ArtistComponent implements OnInit {
   artista:any = {};
+  pistas:any[] = [];
 
   constructor(private activatedRoute:ActivatedRoute,public _spotify:SpotifyService) { 
   }
@@ -17,6 +19,13 @@ export class ArtistComponent implements OnInit {
       this._spotify.getArtista(params['id']).subscribe(artista=>{
         this.artista = artista;
         console.log(artista);
+      });
+
+      this._spotify.getTop(params['id'])
+      .pipe(map((resp:any)=>resp.tracks))
+      .subscribe(pistas=>{
+        console.log(pistas);
+        this.pistas = pistas;
       });
     })  
   }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
   artistas:any[] = [];
   urlSpotify:string = 'https://api.spotify.com/v1/';
-  token:string = 'BQB5l4J2eSZ42SSuNoYFYOWRhF7RenYvGeNRzurzIXEm1otD5PjtoFXx8j-2Pkohv-y_AGB8Ce3R15juKPg';
+  token:string = 'BQBPwK8LfC43JbO2NaPTt-b5I4p-uNCCbsz3XBBWVDjZToiQinxlTjI4bhyGLV0UykUGCzSm4Dj1jaMC47o';
 
   constructor(public http:HttpClient) {
     console.log("Servicio Spotify listo");
@@ -31,6 +31,12 @@ export class SpotifyService {
 
   getArtista(id:string){
     let url = `${this.urlSpotify}artists/${id}`;
+    let headers = this.getHeaders();
+    return this.http.get(url,{headers});
+  }
+
+  getTop(id:string){
+    let url = `${this.urlSpotify}artists/${id}/top-tracks?country=US`;
     let headers = this.getHeaders();
     return this.http.get(url,{headers});
   }
